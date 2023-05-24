58afea2795199ed4a6eff49fa07963a843ae5e29
Livia <liviaads47@gmail.com>
2023-05-23 21:34:50 -0300

    novo repositorio

---
+++ IMC.js
@@ -0,0 +1,8 @@
+//IMC
+let peso = 73;
+let altura = 170;
+
+let alturaM = altura / 100;
+let imc = peso / (alturaM ** 2);
+
+console.log("Seu IMC é de ", imc);
---
+++ celsius.js
@@ -0,0 +1,5 @@
+//conversão Farenheit x Celsius
+let farenheit = 114;
+let celsius = (farenheit - 32) / 1.8;
+
+console.log(celsius);
